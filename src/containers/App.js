ec2a988c63e0e638f94515ef8c1842f0efcfed98
Vijay Kumar <vijaykumar@xeadmins-MacBook-Pro-2.local>
2019-05-30 13:50:21 +0530

    form

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Form, Button, FormControl } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        hell
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  
+
+  render() {
+    return (
+      <div className="container">
+     <Navbar className="bg-light justify-content-between">
+      <Form inline>
+        <FormControl type="text" placeholder="Search" className=" mr-sm-1" />
+        <Button type="submit">Search</Button>
+      </Form>
+    </Navbar>;
+  </div>
+    );
+  }
 }
 
 export default App;
